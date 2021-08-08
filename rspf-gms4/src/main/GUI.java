2fa7fb7d836390dc2ce0db74606ab3867027e0a4
Guilherme Souza <gms4@cin.ufpe.br>
2021-08-08 19:19:31 -0300

    shuffle working

--- rspf-gms4/src/main/GUI.java
+++ rspf-gms4/src/main/GUI.java
@@ -19,12 +19,13 @@ import javax.swing.event.ListSelectionListener;
 
 public class GUI extends player implements ActionListener, ListSelectionListener, WindowConstants {
 
-	private JButton addButton, removeButton, /*pauseButton,*/ playpauseButton, nextButton, previousButton;
+	private JButton addButton, removeButton, playpauseButton, nextButton, previousButton, shuffleButton;
 	private JList<String> setlist; //lista de músicas
 	private JFrame front; //interação com o usuário
-	private JLabel current/*, time, length*/;
+	private JLabel current;
 	int mscPos, mscLength;
 	boolean paused = true;
+	boolean shuffle = false;
 
 	/* Setando aqui os botões necessários e 
 	 * estruturas básicas pra o front funcionar
@@ -55,12 +56,12 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 		playpauseButton = new JButton(">>");
 		playpauseButton.addActionListener(this);
 		playpauseButton.setActionCommand("play/pause");
-		playpauseButton.setBounds(240, 100, 50, 40);
+		playpauseButton.setBounds(250, 100, 50, 40);
 
-		/*playButton = new JButton(">>");
-		playButton.addActionListener(this);
-		playButton.setActionCommand("play");
-		playButton.setBounds(275, 100, 50, 40);*/
+		shuffleButton = new JButton("Shuffle Off");
+		shuffleButton.addActionListener(this);
+		shuffleButton.setActionCommand("shuffle");
+		shuffleButton.setBounds(227, 290, 95, 40);
 
 		setlist = new JList<String>();
 		setlist.setModel(playlist);
@@ -74,15 +75,6 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 		set.setViewportView(setlist);
 		set.setBounds(30, 150, 495, 130);
 
-		//ativando layout
-		/*JPanel layout = new JPanel();
-		layout.setLayout(null);
-		layout.add(addButton);
-		layout.add(removeButton);
-		layout.add(set);*/
-
-		//testei panel e table, ainda decidindo qual vou usar
-
 		JTable layout = new JTable();
 		layout.setLayout(null);
 		layout.add(addButton);
@@ -91,10 +83,8 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 		layout.add(nextButton);
 		layout.add(previousButton);
 		layout.add(playpauseButton);
-		//layout.add(playButton);
 		layout.add(current);
-		//Color c = new Color(0, 60, 0);
-		//layout.setBackground(c);
+		layout.add(shuffleButton);
 
 		//agora, o front
 		front = new JFrame();
@@ -102,7 +92,6 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 		front.setTitle("Eattron Player: 2021 chegou e agora eu sou cringe");
 		front.setSize(570, 400);
 		front.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//front.setBackground(c);
 		front.setVisible(true);
 
 	}
@@ -126,34 +115,39 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 			String length = JOptionPane.showInputDialog(front, "Length", "And how long is it?", -1);
 			initAdd(artist + " - " + music + " (" + length + ")");
 
-		} else if (action.equals("remove")) {
+		} 
+		
+		else if (action.equals("remove")) {
 			//tem que ver se tem algo selecionado pra remover
-			if(isntEmpty()) {
+			if(isSelected()) {
 				initRemove(mscPos);
-				//coloquei a thread pra esperar um pouco depois da exclusão
-				/*try {
-					Thread.sleep(2);
-				} catch (InterruptedException ie) {
-					ie.printStackTrace();
-				}*/
-
 				current.setText("You're listening to: nothing at the moment.");
 			}
 
 			//o next e o previous funciona como uma função normal de previous e next de uma linked list
-		} else if (action.equals("next")) {
-			if(isntEmpty()) {
+		} 
+		
+		else if (action.equals("next")) {
+			
+			if(isSelected()) {
 				mscPos = positionThread(false, 1);
 				select();
 			}
-		} else if (action.equals("previous")) {
-			if(isntEmpty()) {
+			
+		} 
+
+		else if (action.equals("previous")) {
+			
+			if(isSelected()) {
 				mscPos = positionThread(false, -1);
 				select();
 			}
-		} else if (action.equals("play/pause")) {
+			
+		} 
+		
+		else if (action.equals("play/pause")) {
 
-			if (isntEmpty()); {
+			if (isSelected()); {
 				paused = !paused;
 				if (paused) {
 					current.setText(playlist.get(mscPos) + " is paused.");
@@ -163,12 +157,26 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 					playpauseButton.setText("||");
 				}
 			}
+		} 
+		
+		else if (action.equals("shuffle")) {
+
+                if(isSelected()) {
+                	initShuffle();
+                	shuffle = !shuffle;
+                	if (shuffle) {
+                		shuffleButton.setText("Shuffle On");
+                	} else {
+                		shuffleButton.setText("Shuffle Off");
+                	}
+                }
+
 		}
 	}
 
-	private boolean isntEmpty() {
+	private boolean isSelected() {
 		// TODO Auto-generated method stub
-		//se não tiver nada selecionado, retorna null e não roda o remove
+		//se não tiver nada selecionado, retorna null e não roda
 		if (setlist.getSelectedValue() != null) return true;
 		else return false;
 	}
@@ -176,7 +184,7 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 	@Override
 	public void valueChanged(ListSelectionEvent arg0) {
 		// TODO Auto-generated method stub
-		if (isntEmpty()) {
+		if (isSelected()) {
 			mscPos = setlist.getSelectedIndex();
 			positionThread(true, mscPos);
 			startMusic();
@@ -184,7 +192,8 @@ public class GUI extends player implements ActionListener, ListSelectionListener
 	}
 
 	public void startMusic() {
-		if (isntEmpty()) {
+		
+		if (isSelected()) {
 			paused = false;
 			current.setText("You're listening to: " + playlist.get(mscPos));
 			playpauseButton.setText("||");
--- rspf-gms4/src/main/player.java
+++ rspf-gms4/src/main/player.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -12,18 +13,28 @@ public class player {
 	/* Precisamos de um lock pra lidar com a lista de músicas, para
 	 * que nada seja executado de forma precipitada.
 	 * Também precisamos de uma forma de agrupar a lista de músicas
-	 * e as suas espectivas durações. vou usar list model, visto que ele já colabora com o display.
+	 * e as suas respectivas durações. vou usar list model, visto que ele já colabora com o display.
 	 * Vou usar também um boolean pra me auxiliar em saber se tem outra
 	 * thread usando a lista de músicas, porque se sim esperaremos a
 	 * operação acabar. Para sinalizar ações dentro da lista de música, 
 	 * vou criar uma variável da categoria concurrent.condition.
 	 */
 
-	/* Primeira entrega:
+	/* 
+	 * Primeira entrega:
 	 * add (done)
 	 * remove (done)
 	 * listar pra mostrar que add e remove funcionam (done)
 	 * play/pause (done)
+	 * 
+	 * Segunda entrega:
+	 * play/pause (done)
+	 * next/previous (done)
+	 * GUI (done)
+	 * 
+	 * Terceira entrega:
+	 * shuffle (done)
+	 * 
 	 */
 
 	public Lock lock = new ReentrantLock();
@@ -32,6 +43,7 @@ public class player {
 	public DefaultListModel<String> playlist = new DefaultListModel<>();
 	public List<Integer> mscPosOrder = new ArrayList<>(); //listar pra guardar as posições (next/previous)
 	public Condition action = lock.newCondition();
+	public boolean shuffleOn = false;
 
 	//thread 1: adicionar música à lista
 
@@ -97,33 +109,85 @@ public class player {
 	//thread 3: thread que capta a posição da música para fazer o next and previous
 	//ela funciona como thread mas eu quero o id atualizado no return
 	public int positionThread (boolean aux, int index) {
-		
+
+		try {
+
+			lock.lock();
+
+			while (taken) action.await();
+
+			taken = true;
+
+			if (aux) {
+				position = mscPosOrder.indexOf(index);
+			} else {
+				//nova posição vai ser o resto da soma da (nova posição + tamanho da lista + index atual)/tamanho da lista
+				position = (position + mscPosOrder.size() + index) % mscPosOrder.size();
+			}
+
+			taken = false;
+			action.signalAll();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+
+		return mscPosOrder.get(position);
+	}
+
+	/* Thread 4: thread que inicializa a função aleatório do player.
+	 * Reutilizaremos a thread para obter as posições necessárias para fazer com que o shuffle funcione corretamente.
+	 */
+	class shuffleThread extends Thread {
+
+		@Override public void run() {
+
+			int psc = mscPosOrder.get(position);
+
 			try {
 				
 				lock.lock();
 				
 				while (taken) action.await();
-
+				
 				taken = true;
 				
-				if (aux) {
-					position = mscPosOrder.indexOf(index);
+				//se tá ligado, é pra desligar e listar a playlist de forma sequencial
+				if (shuffleOn) {
+					shuffleOn = false;
+					mscPosOrder.clear();
+					for (int i = 0; i < playlist.size(); i++) {
+						mscPosOrder.add(i);
+					}
+					//se tá desligado, é pra ligar e listar a playlist de forma aleatória
 				} else {
-					position = (position + mscPosOrder.size() + index) % mscPosOrder.size();
+					shuffleOn = true;
+					//função para randomizar a ordem dos itens na lista 
+					Collections.shuffle(mscPosOrder);
 				}
+				
+				//em qualquer uma das ocasiões, vamos listar a playlist novamente
+				System.out.println(mscPosOrder);
 				
 				taken = false;
 				action.signalAll();
+				
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} finally {
 				lock.unlock();
 			}
 			
-			return mscPosOrder.get(position);
-		}
+			//precisamos inicializar a thread de posição novamente
+			positionThread(true, psc);
 
+		}
+	}
+    
+	//funções para iniciar as threads
+	
 	public void initAdd(String msc) {
 		Thread add = new Thread(new addThread(msc));
 		add.start();
@@ -133,6 +197,11 @@ public class player {
 		Thread remove = new Thread(new removeThread(pos));
 		remove.start();
 	}
-	
+
+	public void initShuffle() {
+       Thread shuffle = new Thread(new shuffleThread());
+       shuffle.start();
+	}
+
 }
 
